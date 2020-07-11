e98055a0dde875a8507c3408b5df4a50781ea1d6
Arnav Machavarapu <arnav.mach@gmail.com>
2020-07-11 14:24:59 -0500

    installed simple_form and created non-functional tweets form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
 	def main_page
+		@mission_statement = "save the world"
 	end
 
 	def about_us
+		@some_number = 42
 	end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
 	def new
+		@tweet = Tweet.new
+	end
+	def create
 	end
 end
